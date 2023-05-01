4a4de4d806a8b8603c4b6fa418b29e36fb01e694
iliser <i.d.mvmii@gmail.com>
2023-05-01 19:27:05 +0800

    feat: clear rich pressence on when track paused

--- src/discord-rich-pressence.js
+++ src/discord-rich-pressence.js
@@ -10,26 +10,27 @@ const discord = new Client({
 let discordIsReady = false;
 let lastUpdateData = null;
 
-function updatePresence({ track, artist, cover }) {
-  discord.user?.setActivity({
-    state: artist,
-    details: track,
-    largeImageKey: cover?.replace("50x50", "400x400") ?? "yandex-music-icon",
-    // startTimestamp: 1682706503686,
-    // endTimestamp: 1682706503686 + 300 * 1000,
-    // buttons: [
-    //   {
-    //     label: 'Link to track',
-    //     url: 'https://music.yandex.ru/album/20353/track/201155',
-    //   }
-    // ]
-  });
+// rich pressence command
+// type rpc = {type: 'clear'} | {type: 'update',artist,track,cover}
+
+function updatePresence(updateCommand) {
+  const { type } = updateCommand;
+  if (type == "clear") {
+    discord?.user.clearActivity();
+  } else if (type == "update") {
+    const { track, artist, cover } = updateCommand;
+    discord.user?.setActivity({
+      state: artist,
+      details: track,
+      largeImageKey: cover?.replace("50x50", "400x400") ?? "yandex-music-icon",
+    });
+  }
 }
 
 instance.post("/update", async (req, res) => {
   // omit unused parameters
-  const { track, artist, cover } = req.body;
-  const updateData = { track, artist, cover };
+  const { track, artist, cover, type } = req.body;
+  const updateData = { track, artist, cover, type };
   if (!discordIsReady) {
     lastUpdateData = updateData;
     return {};
--- src/tampermonkey-watch-script.js
+++ src/tampermonkey-watch-script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Currrent Played
+// @name         Yandex Music watcher
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Watch yandex music track changes and report it to localhost server
 // @author       iliser
 // @match        https://music.yandex.ru/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.ru
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=music.yandex.ru
 // @connect      localhost
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
@@ -27,31 +27,55 @@
     )?.src;
   }
 
+  function getIsPaused() {
+    return !(
+      window.top.document.querySelector(".player-controls__btn_pause") != null
+    );
+  }
+
   let trackName = null;
   let artistName = null;
+  let isPaused = true;
 
   function update() {
     const track = getTrackName();
     const artist = getArtists();
     const cover = getCover();
+    const _isPaused = getIsPaused();
 
-    if (trackName == track && artistName == artist) return;
+    if (trackName == track && artistName == artist && _isPaused == isPaused)
+      return;
     trackName = track;
     artistName = artist;
+    isPaused = _isPaused;
 
-    var requestDetails = {
-      method: "POST",
-      url: "http://localhost:3333/update",
-      data: JSON.stringify({
-        track,
-        artist,
-        cover,
-      }),
-      headers: { "Content-Type": "application/json" },
-      overrideMimeType: "application/json",
-    };
-    console.log(`Report track to server: ${track} - ${artist}`);
-    GM_xmlhttpRequest(requestDetails);
+    console.log(
+      `Report state to server: ${
+        isPaused ? "pause" : "play"
+      } ${track} - ${artist}`
+    );
+    if (isPaused) {
+      GM_xmlhttpRequest({
+        method: "POST",
+        url: "http://localhost:3333/update",
+        data: JSON.stringify({ type: "clear" }),
+        headers: { "Content-Type": "application/json" },
+        overrideMimeType: "application/json",
+      });
+    } else {
+      GM_xmlhttpRequest({
+        method: "POST",
+        url: "http://localhost:3333/update",
+        data: JSON.stringify({
+          type: "update",
+          track,
+          artist,
+          cover,
+        }),
+        headers: { "Content-Type": "application/json" },
+        overrideMimeType: "application/json",
+      });
+    }
   }
 
   setInterval(update, 5000);
